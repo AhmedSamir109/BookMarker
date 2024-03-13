e30945e75f4d7d3c0739942b06cfbf75663495a3
AhmedSamir109 <ahmedsamir.as9728@gmail.com>
2024-03-13 16:05:45 +0200

    initial-commit

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 var nameInput = document.getElementById('nameInput');
 var urlInput = document.getElementById('urlInput');
 var tbody = document.getElementById('tbody');
-var anchor = document.getElementById('anchor');
+// var anchor = document.getElementById('anchor');
 
 // var sites = [];
 
 
 
-if(localStorage.length !== 0 ){
+//if(localStorage.length !== 0 )
+if(localStorage.getItem('sites')!== null)
+{
     var sites = JSON.parse(localStorage.getItem('sites'));
     displayTable();
 }else{
@@ -74,17 +76,18 @@ function retrivesites(){
 
 
 
-
+var index = 0 ;
 
 function displayTable(){
     var trs = "" ;
 
     for(var i = 0 ; i<sites.length ; i++){
+        index = i ;
         trs += `<tr>
                     <td>${i+1}</td>
                     <td>${sites[i].sitaName}</td>
                     <td>
-                    <button class="btn btn-dark px-4" onclick="visitSite(${i})">
+                    <button class="btn btn-dark px-4"  id="visit-btn">
                         <i class="fa-solid fa-eye px-1 "></i> Visit
                     </button>
                     </td>
@@ -104,7 +107,6 @@ function displayTable(){
 
 
 
-
 function deleteBookmark(index) {
     sites.splice(index , 1)
     localStorage.setItem('sites' , JSON.stringify(sites))
@@ -113,6 +115,33 @@ function deleteBookmark(index) {
 
 function visitSite(index){
     //  anchor.innerHTML= sites[index].urlInput;
-  window.open(sites[index].siteUrl , "_blank" )
+    window.open(sites[index].siteUrl , "_blank" )
   
+}
+
+
+
+
+// var visit = document.querySelector('.btn-dark');
+
+// visit.addEventListener('click',function(){
+
+//     function visitSite(index){
+//         //  anchor.innerHTML= sites[index].urlInput;
+//     //   window.open(sites[index].siteUrl , "_blank" )
+//     visit.setAttribute('target','_blank')
+      
+//     }
+    
+    
+// })
+
+var visitBtns = document.querySelectorAll('#visit-btn')
+for(var i = 0 ; i<visitBtns.length ; i++){
+
+    visitBtns[i].addEventListener('click',function(e){
+        visitSite(index)
+        console.log(e.target)
+    })
+
 }
